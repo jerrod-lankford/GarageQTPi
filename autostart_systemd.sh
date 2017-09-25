a1a31d5ff501154550546bee2f2eb7ff142c859d
tagno25 <tagno25@gmail.com>
2017-09-24 20:04:54 -0500

    fix readability

--- autostart_systemd.sh
+++ autostart_systemd.sh
@@ -2,4 +2,4 @@
 cp garageqtpi@pi.service /etc/systemd/system/garageqtpi@`whoami`.service
 sed -i "s?/home/pi/GarageQTPi?`pwd`?" /etc/systemd/system/garageqtpi@`whoami`.service
 systemctl --system daemon-reload
-systemctl enable home-assistant@`whoami`
+systemctl enable home-assistant@`whoami`.service
