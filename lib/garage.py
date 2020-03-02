83f73be0bb39a42b4fdfd2a87349c85e49bacf75
Bob Gray <bobgray1000@gmail.com>
2020-03-02 05:44:05 -0500

    formatting changes for pep8

--- lib/garage.py
+++ lib/garage.py
@@ -3,15 +3,17 @@ import RPi.GPIO as GPIO
 from .eventhook import EventHook
 
 
-SHORT_WAIT = .2 #S (200ms)
+SHORT_WAIT = .2  # S (200ms)
 """
-    The purpose of this class is to map the idea of a garage door to the pinouts on 
+    The purpose of this class is to map the idea of a garage door to the pinouts on
     the raspberrypi. It provides methods to control the garage door and also provides
     and event hook to notify you of the state change. It also doesn't maintain any
     state internally but rather relies directly on reading the pin.
 """
+
+
 class GarageDoor(object):
-    
+
     def __init__(self, config):
 
         # Config
@@ -25,13 +27,17 @@ class GarageDoor(object):
         self._state = None
         self.onStateChange = EventHook()
 
-        # Set relay pin to output, state pin to input, and add a change listener to the state pin
+        # Set relay pin to output, state pin to input, and add a change
+        # listener to the state pin
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_pin, GPIO.OUT)
         GPIO.setup(self.state_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.state_pin, GPIO.BOTH, callback=self.__stateChanged, bouncetime=300)
-
+        GPIO.add_event_detect(
+            self.state_pin,
+            GPIO.BOTH,
+            callback=self.__stateChanged,
+            bouncetime=300)
 
         # Set default relay state to false (off)
         GPIO.output(self.relay_pin, self.invert_relay)
@@ -61,7 +67,7 @@ class GarageDoor(object):
         # Read the mode from the config. Then compare the mode to the current state. IE. If the circuit is normally closed and the state is 1 then the circuit is closed.
         # and vice versa for normally open
         state = GPIO.input(self.state_pin)
-        if  state == self.mode:
+        if state == self.mode:
             return 'closed'
         else:
             return 'open'
@@ -72,8 +78,8 @@ class GarageDoor(object):
         time.sleep(SHORT_WAIT)
         GPIO.output(self.relay_pin, self.invert_relay)
 
-   
     # Provide an event for when the state pin changes
+
     def __stateChanged(self, channel):
         if channel == self.state_pin:
             # Had some issues getting an accurate value so we are going to wait for a short timeout
@@ -83,10 +89,12 @@ class GarageDoor(object):
 
 #
 # This class inherits from GarageDoor and make use of a second (open) switch
-# to enable states: closed, opening, open, closing 
+# to enable states: closed, opening, open, closing
 # instead of just open and closed.
-# Note that unlike it's parent class this class does maintain an internal state 
+# Note that unlike it's parent class this class does maintain an internal state
 #
+
+
 class TwoSwitchGarageDoor(GarageDoor):
 
     def __init__(self, config):
@@ -96,14 +104,20 @@ class TwoSwitchGarageDoor(GarageDoor):
         self.open_pin = config['open']
         # Add event detect for the open pin
         GPIO.setup(self.open_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.open_pin, GPIO.BOTH, callback=self.__stateChanged, bouncetime=300)
-        
-    # State is a read only property. It's value is determined by the previous state and what just happened with the switches
+        GPIO.add_event_detect(
+            self.open_pin,
+            GPIO.BOTH,
+            callback=self.__stateChanged,
+            bouncetime=300)
+
+    # State is a read only property. It's value is determined by the previous
+    # state and what just happened with the switches
     @property
     def state(self):
         # Read the mode from the config. Then compare the mode to the current state. IE. If the circuit is normally closed and the state is 1 then the circuit is closed.
         # and vice versa for normally open
-        closed_state = True if GPIO.input(self.state_pin) == self.mode else False
+        closed_state = True if GPIO.input(
+            self.state_pin) == self.mode else False
         open_state = True if GPIO.input(self.open_pin) == self.mode else False
         if closed_state:
             self._state = 'closed'
@@ -114,7 +128,7 @@ class TwoSwitchGarageDoor(GarageDoor):
         elif self._state == 'open':
             self._state = 'closing'
         return self._state
-        
+
         # Provide an event for when the state pin changes
     def __stateChanged(self, channel):
         if channel == self.state_pin or channel == self.open_pin:
@@ -122,5 +136,3 @@ class TwoSwitchGarageDoor(GarageDoor):
             # after a statechange and then grab the state
             time.sleep(SHORT_WAIT)
             self.onStateChange.fire(self.state)
-        
-
